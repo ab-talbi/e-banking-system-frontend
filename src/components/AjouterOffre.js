4497f6ddb9e5200ce9fd97b36a4e72122ac54e88
ayoub <ayoubtalbi787@gmail.com>
2023-09-15 04:30:49 +0100

    feat: axios post offre avec affichage de Toast

--- src/components/AjouterOffre.js
+++ src/components/AjouterOffre.js
@@ -1,60 +1,110 @@
 import React, {Component} from "react";
 import { Card, Form, Button, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faSave, faPlusSquare} from "@fortawesome/free-solid-svg-icons";
+import {faSave, faPlusSquare, faUndo} from "@fortawesome/free-solid-svg-icons";
+import MessageToast from "./MessageToast";
+
+import axios from "axios";
+
+const CREATE_OFFRE_REST_API_URL = 'http://localhost:8089/api/offres';
 
 export default class AjouterOffre extends Component{
 
     constructor(props){
         super(props);
-        this.state = {libelle:'', description:''};
+        this.state = this.initialState;
+        this.state.show = false;
+        this.state.successOuDanger = 'success';
         this.offreChange = this.offreChange.bind(this);
         this.ajouterOffre = this.ajouterOffre.bind(this);
     }
 
-    ajouterOffre(e){
-        alert("Libelle : "+this.state.libelle+", Description : "+this.state.description);
-        e.preventDefault();
+    initialState = {
+        libelle: '',
+        description: ''
     }
 
-    offreChange(e) {
+    ajouterOffre = e => {
+        e.preventDefault();
+
+        const offre = {
+            libelle : this.state.libelle,
+            description : this.state.description
+        }
+
+        axios.post(CREATE_OFFRE_REST_API_URL,offre)
+            .then(response => {
+                if(response.data != null){
+                    console.log(response.data);
+                    this.resetOffre()
+                    this.setState({'show':true});
+                    this.setState({'successOuDanger':'success'});
+                    setTimeout(()=>this.setState({'show':false}), 4000)
+                }else{
+                    this.setState({'show':false});
+                }
+            }).catch(err => {
+                console.log(err);
+                this.setState({'show':true});
+                this.setState({'successOuDanger':'danger'});
+                setTimeout(()=>this.setState({'show':false}), 4000)
+            })
+    }
+
+    offreChange = e => {
         this.setState({[e.target.name]: e.target.value});
     }
 
+    resetOffre = () => {
+        this.setState(() => this.initialState)
+    }
+
     render(){
+
+        const {libelle, description} = this.state;
+        const show = this.state.show;
+        const successOuDanger = this.state.successOuDanger;
+        const message = successOuDanger === 'success' ? "L'offre est enregistré par succés" : "Tres long...";
+
         return (
-            <Card className={"border border-dark bg-dark text-white"}>
-                <Card.Header><FontAwesomeIcon icon={faPlusSquare} /> Ajouter un Offre</Card.Header>
-                <Form onSubmit={this.ajouterOffre} id="offreFormId" className="form-control border border-dark bg-dark text-white">
-                    <Card.Body>
-                        <Row>
-                            <Form.Group as={Col} controlId="formLibelle" className="mb-3" xs={12} md={4}>
-                                <Form.Label>Libellé</Form.Label>
-                                <Form.Control 
-                                    type="text" name="libelle"
-                                    value={this.state.libelle}
-                                    onChange={this.offreChange}
-                                    className={"border border-warning bg-dark text-white"}
-                                    placeholder="Libellé de l'offre" required />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId="formDescription" className="mb-3" xs={12} md={8}>
-                                <Form.Label>Description</Form.Label>
-                                <Form.Control
-                                    as="textarea" rows={3} name="description"
-                                    value={this.state.description}
-                                    onChange={this.offreChange}
-                                    className={"border border-warning bg-dark text-white"}
-                                    placeholder="Description de l'offre" required />
-                            </Form.Group>
-                        </Row>
-                    </Card.Body>
-                    <Card.Footer style={{"textAlign":"right"}}>
-                        <Button size="sm" variant="warning" type="submit">
-                            <FontAwesomeIcon icon={faSave} />  Enregistrer
-                        </Button>
-                    </Card.Footer>
-                </Form>
-            </Card>
+            <div>
+                <div style={{"display":this.state.show ? 'block' : 'none'}}>
+                    <MessageToast children={{show:show, header:successOuDanger, message:message}} />
+                </div>
+                <Card className={"border border-dark bg-dark text-white"}>
+                    <Card.Header><FontAwesomeIcon icon={faPlusSquare} /> Ajouter un Offre</Card.Header>
+                    <Form onReset={this.resetOffre} onSubmit={this.ajouterOffre} id="offreFormId" className="form-control border border-dark bg-dark text-white">
+                        <Card.Body>
+                            <Row>
+                                <Form.Group as={Col} controlId="formLibelle" className="mb-3" xs={12} md={4}>
+                                    <Form.Label>Libellé</Form.Label>
+                                    <Form.Control 
+                                        type="text" name="libelle" autoComplete="off"
+                                        value={libelle} onChange={this.offreChange}
+                                        className={"border border-warning bg-dark text-white"}
+                                        placeholder="Libellé de l'offre" required />
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formDescription" className="mb-3" xs={12} md={8}>
+                                    <Form.Label>Description</Form.Label>
+                                    <Form.Control
+                                        as="textarea" rows={3} name="description" autoComplete="off"
+                                        value={description} onChange={this.offreChange}
+                                        className={"border border-warning bg-dark text-white"}
+                                        placeholder="Description de l'offre" required />
+                                </Form.Group>
+                            </Row>
+                        </Card.Body>
+                        <Card.Footer style={{"textAlign":"right"}}>
+                            <Button size="sm" variant="dark" type="reset">
+                                <FontAwesomeIcon icon={faUndo} />  Réinitialiser
+                            </Button>{' '}
+                            <Button size="sm" variant="warning" type="submit">
+                                <FontAwesomeIcon icon={faSave} />  Enregistrer
+                            </Button>
+                        </Card.Footer>
+                    </Form>
+                </Card>
+            </div>
         );
     }
 }
---
+++ src/components/MessageToast.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import { Toast } from "react-bootstrap";
+import {ToastContainer} from "react-bootstrap";
+
+export default class MessageToast extends Component{
+
+    render(){
+
+        const styles={
+            zIndex:'1'
+        };
+
+        const show = this.props.children.show;
+        const message = this.props.children.message;
+        const header = this.props.children.header === 'success' ? 'Success' : 'Erreur';
+        const classBackground = this.props.children.header === 'success' ? 'bg-success' : 'bg-danger';
+
+        return (
+            <div>
+                <ToastContainer
+                    className="p-3"
+                    position={'bottom-start'}
+                    style={show?styles:null} >
+                    <Toast className={"border border-warning bg-dark text-white"} show={show}>
+                        <Toast.Header className={classBackground + " text-white"} closeButton={false}>
+                            <strong className="me-auto">{header}</strong>
+                        </Toast.Header>
+                        <Toast.Body>
+                            {message}
+                        </Toast.Body>
+                    </Toast>
+                </ToastContainer>
+            </div>
+        );
+    }
+}
